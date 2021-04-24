aeaf14afdf89140147004ad982039c87c6cab588
brodyreid <brodyhreid@gmail.com>
2021-04-24 16:57:25 -0700

    changed Migration for Drillz and added working Seed

--- routes/drills.js
+++ routes/drills.js
@@ -4,39 +4,38 @@ let router = express.Router();
 
 
 router
-.route('')
-.get((req, res) => {
-    return knex('drillz')
-        .orderBy('created_at', 'DESC')
-        .select('*')
-        .then(drillz => {
-            res.render('drills/index', { drillz })
+    .route('')
+    .get((req, res) => {
+        return knex('drillz')
+            .orderBy('created_at', 'DESC')
+            .select('*')
+            .then(drillz => {
+                res.render('drills/index', { drillz })
+        })
     })
-})
+    .post((req, res) => {
 
-.post((req, res) => {
+        const { title, description } = req.body
 
-    const { title, description } = req.body
+        knex('drillz')
+        .insert(
+            {
+                title: title,
+                description: description,
+            }, '*')
+        .then((data) => {
+            //res.render(data)
+            console.table(data)
+        })
+        .catch(console.error)
+        res.redirect('/drills')
+    });
 
-    knex('drillz')
-    .insert(
-        {
-            title: title,
-            description: description,
-        }, '*')
-    .then((data) => {
-        //res.render(data)
-        console.table(data)
+    router
+    .route('/new')
+    .get((request, response) => {
+        response.render('drills/new')
     })
-    .catch(console.error)
-    res.redirect('/drills')
-});
-
-router
-.route('/new')
-.get((request, response) => {
-    response.render('drills/new')
-})
 
 router
     .route("/:id")
