554cd63a0c271d5ef99a83bc36b61bb9ce7e1d84
jose <jmvr37@hotmail.com>
2021-04-24 14:53:51 -0700

    add index page for drills

--- routes/drills.js
+++ routes/drills.js
@@ -2,32 +2,42 @@ const express = require("express");
 const knex = require('../db/client')
 let router = express.Router();
 
-router.route("/:id")
-    .get((req, res) => {
-        res.write("show drill")
-    })
-    // .post((req, res) => {
-        
-    // })
-    // .put((req, res) => {
-        
-    // })
-    // .delete((req, res) => {
-        
-    // });
 
-// router.route("/:id/exercises")
-//     .get((req, res) => {
-//         ...
-//     })
-//     .post((req, res) => {
-//         ...
-//     })
-//     .put((req, res) => {
-//         ...
-//     })
-//     .delete((req, res) => {
-//         ...
-//     });
+router
+.route('')
+.get((req, res) => {
+    return knex('drillz')
+        .orderBy('created_at', 'DESC')
+        .select('*')
+        .then(drillz => {
+            res.render('drills/index', { drillz })
+    })
+})
+
+.post((req, res) => {
+
+    const { title, description } = req.body
+
+    knex('drillz')
+    .insert(
+        {
+            title: title,
+            description: description,
+        }, '*')
+    .then((data) => {
+        //res.render(data)
+        console.table(data)
+    })
+    .catch(console.error)
+    res.redirect('/drills')
+});
+
+router
+.route('/new')
+.get((request, response) => {
+    response.render('drills/new')
+})
+
+
 
 module.exports = router;
