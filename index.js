9f52fe87854803fcb395717f51a239f455c42a6c
Omar <o.a.1027@outlook.com>
2021-04-23 22:30:57 -0700

    session functionality

--- index.js
+++ index.js
@@ -20,42 +20,6 @@ app.set("view engine", "ejs")
 app.use(logger("dev"));
 
 
-// app.get('/sign_up', (req, res) => {
-//     res.render('sign_up')
-// })
-
-
-// CREATING AN ACCOUNT 
-// app.post('/thank_you', (request, response)=>{
-
-//     const {f_name, l_name, email, password, password_confirmation} = request.body;
-
-//     // CHECKS FOR PASSWORD CONFIRMATION IF THEY DON'T MATCH IT SENDS THEM TO SIMPLE ERROR PAGE 
-//     if(password === password_confirmation) {
-//         knex('users')
-//         .insert({
-//             first_name: f_name,
-//             last_name: l_name,
-//             email: email,
-//             password: password
-//         }, '*')
-//         .then(data => {
-//             console.log(data);
-//             response.render('thank_you');
-//         })
-//         .catch(console.error);
-//     } else {
-//         response.send('<h1> The password confirmation did not match, try again!</h1>')
-//     }
-// })
-
-
-// SIGN IN PAGE
-
-// app.get("/sign_in", (request, response) =>{
-//     response.render("sign_in")
-// })
-
 
 app.use('/', rootRouter)
 app.use('/users', usersRouter)
--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,26 @@ router.get('/new', (request, response) => {
     response.render('users/new')
 })
 
+router.post('/', async (request, response) => {
+    const {f_name, l_name, email, password} = request.body
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const [ user ] = await knex('users')
+    .insert({
+      first_name: f_name,
+      last_name: l_name,
+      email: email,
+      password: hashedPassword,
+    }, '*')
+    .catch(err => {
+        console.log(err)
+      response.redirect('/users/new')
+    })
+
+    response.redirect('/')
+
+})
+
 
 module.exports = router
